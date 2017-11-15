5c9d9e81b11b384f611d1208c3c3415277e348e1
michaelfrank <michaelfrankbusiness@gmail.com>
2017-11-15 16:34:25 -0500

    Navigation changed for mobile users and added javascript file

--- blog.js
+++ blog.js
@@ -1,3 +1,23 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-}
+var menu = document.getElementById("hamburgerMenu"),
+ham1 = document.getElementById("line1"),
+ham2 = document.getElementById("line2"),
+ham3 = document.getElementById("line3");
+
+var menuStyle = document.getElementById("dropContent");
+menuStyle.style.display="none";
+
+var contentDrop = document.getElementById("dropContent"),
+dropMenu = document.getElementById("dropdownMenu");
+
+menu.addEventListener("click", function(){
+    if(menuStyle.style.display=="none"){
+        menuStyle.style.display = "block";
+        document.body.style.backgroundColor="grey";
+    }
+    else{
+        menuStyle.style.display="none";
+        document.body.style.backgroundColor="white";
+    }
+});
+
+
